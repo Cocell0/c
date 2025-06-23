58cf958107bf617e59773157cb5d54ad3172454f
Cocell0 <cocell.zen.tepys@gmail.com>
2025-06-23 21:51:12 +0600

    Update

--- packages/admin-password/main.ts
+++ packages/admin-password/main.ts
@@ -1,1 +1,4 @@
 import 'virtual:uno.css'
+import 'core/src/base.css'
+import 'core/src/component.css'
+import './assets/styles.scss'
--- packages/admin-password/vite.config.ts
+++ packages/admin-password/vite.config.ts
@@ -1,10 +1,12 @@
-import UnoCSS from 'unocss/vite'
 import { defineConfig } from 'vite'
-import { viteSingleFile } from "vite-plugin-singlefile"
+import UnoCSS from 'unocss/vite'
+import { viteSingleFile } from 'vite-plugin-singlefile'
+import inlineSource from "vite-plugin-inline-source";
 
 export default defineConfig({
   plugins: [
     UnoCSS(),
     viteSingleFile(),
+    inlineSource(),
   ],
 })

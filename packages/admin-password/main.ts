270f6112005f55337c3bb9801e6302c272063847
Cocell0 <cocell.zen.tepys@gmail.com>
2025-06-24 11:48:37 +0600

    Imported initialization module

--- packages/admin-password/main.ts
+++ packages/admin-password/main.ts
@@ -2,3 +2,8 @@ import 'virtual:uno.css'
 import 'core/src/base.css'
 import 'core/src/component.css'
 import './assets/styles.scss'
+
+import { name } from './package.json' assert { type: 'json' };
+import { initialize } from 'core'
+
+initialize(name);

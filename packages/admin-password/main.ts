d33f0b964c8b6d30b7943388f7ea7afd107f283a
Cocell0 <cocell.zen.tepys@gmail.com>
2025-06-24 20:09:29 +0600

    Update

--- packages/admin-password/main.ts
+++ packages/admin-password/main.ts
@@ -3,7 +3,11 @@ import 'core/src/base.css'
 import 'core/src/component.css'
 import './assets/styles.scss'
 
+// import { locate } from '@iconify/json';
 import { name } from './package.json' assert { type: 'json' };
 import { initialize } from 'core'
 
 initialize(name);
+
+
+// const materialSymbols = locate('material-symbols');

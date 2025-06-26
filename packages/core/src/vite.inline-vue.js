c463a36d6fbaf6cc19823691cfa14e04b4686f41
Coc̀éll <cocell.zen.tepys@gmail.com>
2025-06-27 03:15:18 +0600

    Modified plugin to target `<!-- Vue UMD -->` to be replaced with the proper Vue UMD script

--- packages/core/src/vite.inline-vue.js
+++ packages/core/src/vite.inline-vue.js
@@ -2,12 +2,10 @@ export default function inlineVue() {
   return {
     name: 'inline-vue',
     async transformIndexHtml(html) {
-      // import Node modules only when running in build (Node) context
       const { readFile } = await import('fs/promises')
       const { fileURLToPath } = await import('url')
       const { dirname, join } = await import('path')
 
-      // Resolve path relative to this plugin file
       const __filename = fileURLToPath(import.meta.url)
       const __dirname = dirname(__filename)
       const vuePath = join(__dirname, '../../core/lib/vue.global.prod.js')
@@ -15,7 +13,7 @@ export default function inlineVue() {
       const vueScript = await readFile(vuePath, 'utf-8')
 
       return html.replace(
-        /<script src="https:\/\/unpkg\.com\/vue@3(?:\.5\.17)?\/dist\/vue\.global\.js"><\/script>/,
+        /<!-- Vue UMD -->/,
         `<script>${vueScript}</script>`
       )
     }

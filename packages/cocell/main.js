7e74344d4b7bb01104d65bac85bcaa93d57a1c62
Coc̀éll <cocell.zen.tepys@gmail.com>
2025-06-28 16:10:25 +0600

    Removed Cocell

--- packages/cocell/main.js
+++
@@ -1,8 +0,0 @@
-import 'virtual:uno.css'
-import 'core/src/base.css'
-import 'core/src/component.css'
-
-import { name } from './package.json';
-import { initialize } from 'core'
-
-initialize(name);

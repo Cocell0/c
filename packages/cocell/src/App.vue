e74adca45d137899f60f48921b65bb1c734cce7a
Coc̀éll <cocell.zen.tepys@gmail.com>
2025-06-30 19:43:47 +0600

    Refactored generator creation in seperate component

--- packages/cocell/src/App.vue
+++ packages/cocell/src/App.vue
@@ -1,29 +1,23 @@
 <template>
-  <div :hidden="!generatorStats.length" class="generators">
+  <div :hidden="!generators.length" class="generators">
     <h2 class="text-center">Generators</h2>
     <ul>
-      <li v-for="(generator, index) in generatorStats" :key="index">
-        <a class="button" :href="`http://perchance.org/${generator.name}`">
-          <img class="thumbnail"
-            :src="`https://perchance.org/api/getGeneratorScreenshot?generatorName=${generator.name}`" />
-          <div class="detail">
-            <h4 class="m0">{{ generator.metaData.title || generator.name }}</h4>
-          </div>
-        </a>
-      </li>
+      <Generator v-for="(name, index) in generators" :key="index" :name="name" />
     </ul>
   </div>
-  <div class="chat" v-html="chatHtml"></div>
+
+  <h2 class="text-center my-4">Hub Chat</h2>
+  <div class="perchance-chat-container" v-html="chat"></div>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref } from 'vue'
 import { computed } from 'vue'
+import Generator from './components/Generator.vue'
 
-const chatHtml = computed(() => (window.chat || (() => 'Chat not defined'))())
+const chat = computed(window.chat)
 
 const generators = ref([])
-const generatorStats = ref([])
 
 if (window.generators) {
   generators.value = Object.values(window.generators).map(g => g.$text)
@@ -37,13 +31,4 @@ if (generators.value.length === 0) {
     'admin-password'
   ]
 }
-
-onMounted(async () => {
-  const fetchStats = name =>
-    fetch(`https://perchance.org/api/getGeneratorStats?name=${encodeURIComponent(name)}`)
-      .then(res => res.json())
-      .then(json => json.data)
-
-  generatorStats.value = await Promise.all(generators.value.map(fetchStats))
-})
 </script>

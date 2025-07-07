87cd3db36683f166bba6ccdd77e7f1965bbf5497
Coc̀éll <cocell.zen.tepys@gmail.com>
2025-07-07 13:18:05 +0600

    Added conditions to check project location and open original location

--- packages/core/src/initialize.js
+++ packages/core/src/initialize.js
@@ -1,14 +1,22 @@
 function initialize(name) {
-  if (window.location.href.includes(`perchance.org/${name}`)) {
-    const baseElement = document.querySelector('base');
+  if (window.location.href.includes('perchance.org/')) {
+    // console.log('Location includes perchance.org/')
+    if (window.location.href.includes(`perchance.org/${name}`)) {
+      // console.log(`Location includes perchance.org/${name}`)
+      const baseElement = document.querySelector('base');
 
-    if (baseElement) {
-      baseElement.href = `https://perchance.org/${name}`;
+      if (baseElement) {
+        baseElement.href = `https://perchance.org/${name}`;
+      } else {
+        const baseElement = document.createElement("base");
+        baseElement.href = `https://perchance.org/${name}`;
+
+        document.head.prepend(baseElement)
+      }
     } else {
-      const baseElement = document.createElement("base");
-      baseElement.href = `https://perchance.org/${name}`;
-
-      document.head.prepend(baseElement);
+      // console.log(`Location does NOT includes perchance.org/${name}`)
+      alert(`Project location invalid, opening original location:\nperchance.org/${name}`)
+      window.open(`https://perchance.org/${name}`, '_self')
     }
   }
 }

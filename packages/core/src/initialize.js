ee926ee7eb0b92d0358adb0ec78e5601f1a6a992
Coc̀éll <cocell.zen.tepys@gmail.com>
2025-07-08 19:56:46 +0600

    Add logic to load Work Sans and Roboto Mono font

--- packages/core/src/initialize.js
+++ packages/core/src/initialize.js
@@ -19,6 +19,10 @@ function initialize(name) {
       window.open(`https://perchance.org/${name}`, '_self')
     }
   }
+
+  const fontString = '<link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">'
+
+  document.head.innerHTML += fontString;
 }
 
 export { initialize };

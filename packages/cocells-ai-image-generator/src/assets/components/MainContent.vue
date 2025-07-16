7fb10fa58ef1ffc09c39be7c0bc199a78c5694fb
Coc̀éll <cocell.zen.tepys@gmail.com>
2025-07-16 10:01:01 +0600

    Add responsive prompt area with dynamic textarea resizing
    Introduces a new component to manage a prompt input area with dynamic resizing of the textarea based on user input.
    
    Includes a virtual textarea for accurate scroll height calculation to prevent issues caused by transitions.
    
    Adds buttons for configuration toggling and image creation, enhancing interactivity.

---
+++ packages/cocells-ai-image-generator/src/assets/components/MainContent.vue
@@ -0,0 +1,46 @@
+<template>
+  <main>
+    <div class="prompt-area" @click="focusPromptBox">
+      <div class="prompt-container">
+        <textarea name="prompt" id="prompt" ref="promptBox"></textarea>
+        <!-- 
+        A virtual text area to perform accurate measurement of the scroll height needed for the original text area
+        This is done because the original text area has transitions which can cause incorrect scroll height calculations due to interpolation
+          -->
+        <textarea name="virtual-prompt" class="virtual-prompt" ref="virtualPromptBox" readonly></textarea>
+      </div>
+      <div class="option-container">
+        <button class="button--icon" aria-label="Open configurations" @click="toggleConfigurationbar">
+          <span class="i-material-symbols-tune-rounded"></span>
+        </button>
+        <button class="button--icon" aria-label="Create image">
+          <span class="i-material-symbols-add-rounded"></span>
+        </button>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script setup>
+import { ref, onMounted, defineEmits } from 'vue'
+
+const emit = defineEmits(['toggle-configuration-bar']);
+
+const toggleConfigurationbar = () => {
+  emit('toggle-configuration-bar')
+};
+
+const promptBox = ref(null)
+const virtualPromptBox = ref(null)
+
+onMounted(() => {
+  promptBox.value.addEventListener('input', () => {
+    if (!promptBox.value) return
+
+    virtualPromptBox.value.value = promptBox.value.value
+
+    promptBox.value.style.height = 'auto'
+    promptBox.value.style.height = `${virtualPromptBox.value.scrollHeight}px`
+  })
+})
+</script>

e9d72f23e518d12f1f5f964d12752b6aecf245f9
Coc̀éll <cocell.zen.tepys@gmail.com>
2025-07-16 09:57:29 +0600

    Import `paramount.css` from Core

--- packages/cocells-ai-image-generator/src/main.js
+++ packages/cocells-ai-image-generator/src/main.js
@@ -2,6 +2,7 @@ import 'virtual:uno.css'
 import 'core/src/base.css'
 import 'core/src/component.css'
 import './assets/main.scss'
+import 'core/src/paramount.css'
 
 import { name } from '../package.json';
 import { initialize } from 'core'

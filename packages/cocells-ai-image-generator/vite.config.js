987e1db2c2c665a8e34bb8ca1e2f53af40d1b1b8
Coc̀éll <cocell.zen.tepys@gmail.com>
2025-07-17 19:08:28 +0600

    Remove Vue dev tools

--- packages/cocells-ai-image-generator/vite.config.js
+++ packages/cocells-ai-image-generator/vite.config.js
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite'
 import UnoCSS from 'unocss/vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 import { viteSingleFile } from 'vite-plugin-singlefile'
 
 export default defineConfig({
   plugins: [
     UnoCSS(),
     vue(),
-    vueDevTools(),
     viteSingleFile(),
   ],
 })

c5583a88189a19b9642ddfb4f8e1a6a11130a72f
Coc̀éll <cocell.zen.tepys@gmail.com>
2025-07-19 22:06:46 +0600

    Add image component

---
+++ packages/cocells-ai-image-generator/src/components/Image.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="image-container">
+    <img :src="image" :alt="`Generated Image ${prompt}`" />
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const image = ref('')
+const props = defineProps({
+  prompt: {
+    type: String,
+    required: true
+  }
+})
+
+</script>

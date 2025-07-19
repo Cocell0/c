2608d05beb9d7eba55b5a1f8f77e4eda7790856b
Coc̀éll <cocell.zen.tepys@gmail.com>
2025-07-19 10:47:47 +0600

    Refactor layout

--- packages/cocells-ai-image-generator/src/App.vue
+++ packages/cocells-ai-image-generator/src/App.vue
@@ -1,27 +1,22 @@
 <template>
-  <div class="backdrop" @click="closeSidebars" :class="{ 'backdrop--show': state.backdrop }"></div>
   <div class="panel">
     <div class="drawer">
-      <button @click="toggleNavigationBar" class="button--icon"
-        :aria-label="state.openNavigationBar ? 'Close sidebar' : 'Open sidebar'">
+      <button @click="toggleNavigationBar" class="button--icon" :class="{ 'opacity-0': hideMenuButton }"
+        :inert="hideMenuButton" :aria-label="state.openNavigationBar ? 'Close sidebar' : 'Open sidebar'">
         <span
           :class="{ 'i-material-symbols-menu-open-rounded': state.openNavigationBar, 'i-material-symbols-menu-rounded': !state.openNavigationBar }"></span>
       </button>
-      <button @click="toggleRightSidebar" class="button--icon"
-        :aria-label="state.openConfigurationBar ? 'Close sidebar' : 'Open sidebar'">
-        <span
-          :class="{ 'i-material-symbols-menu-open-rounded': state.openConfigurationBar, 'i-material-symbols-menu-rounded': !state.openConfigurationBar }"></span>
-      </button>
     </div>
 
-    <Sidebar role="navigation" :open="state.openNavigationBar" class="sidebar--left navigation-bar">
-      <button>hi</button>
-    </Sidebar>
+    <Sidebar ref="navigationSidebar" @close="toggleNavigationBar" role="navigation" :open="state.openNavigationBar" class="navigation-bar"></Sidebar>
   </div>
-  <MainContent></MainContent>
+  <MainContent @toggle-configuration-bar="toggleConfigurationBar"></MainContent>
   <div class="panel">
-    <Sidebar :open="state.openConfigurationBar" class="sidebar--right configuration-bar">
-      <h2>Right Sidebar</h2>
+    <Sidebar ref="configurationSidebar" @close="toggleConfigurationBar" :open="state.openConfigurationBar" class="configuration-bar relative" position="right">
+      <button class="button--icon close-button" aria-label="Close configuration" @click="toggleConfigurationBar">
+        <span class="i-material-symbols-close-rounded"></span>
+      </button>
+      <h2 class="mt-2.5rem">Right Sidebar</h2>
       <p>This is the right sidebar content.</p>
     </Sidebar>
   </div>
@@ -30,48 +25,33 @@
 <script setup>
 import Sidebar from 'core/src/vue/components/Sidebar.vue';
 import MainContent from 'cocells-ai-image-generator/src/assets/components/MainContent.vue';
-import { ref } from 'vue';
+import { ref, onMounted, computed } from 'vue';
 
 const state = ref({
   openNavigationBar: false,
   openConfigurationBar: false,
-  backdrop: false,
+  screenWidth: ref(window.innerWidth),
 });
 
-const toggleNavigationBar = () => {
+onMounted(() => window.addEventListener('resize', () => {
+  state.value.screenWidth = window.innerWidth
+}))
+
+const hideMenuButton = computed(() =>
+  state.value.screenWidth <= 768 && state.value.openConfigurationBar
+)
+
+function toggleNavigationBar() {
   state.value.openNavigationBar = !state.value.openNavigationBar;
-
-  if (window.innerWidth <= 768 && (state.value.openNavigationBar || state.value.openConfigurationBar)) {
-    state.value.backdrop = state.value.openNavigationBar || state.value.openConfigurationBar;
-  } else {
-    state.value.backdrop = false;
-  }
-};
-
-const toggleRightSidebar = () => {
-  state.value.openConfigurationBar = !state.value.openConfigurationBar;
-
-  if (window.innerWidth <= 768 && (state.value.openNavigationBar || state.value.openConfigurationBar)) {
-    state.value.backdrop = state.value.openNavigationBar || state.value.openConfigurationBar;
-  } else {
-    state.value.backdrop = false;
-  }
-};
-
-const closeSidebars = () => {
-  state.value.openNavigationBar = false;
-  state.value.openConfigurationBar = false;
-  state.value.backdrop = false;
-};
-
-window.addEventListener('resize', () => {
-  if (window.innerWidth > 768) {
-    if (state.value.openNavigationBar) state.value.openNavigationBar = true;
-    if (state.value.openConfigurationBar) state.value.openConfigurationBar = true;
-  } else {
-    state.value.openNavigationBar = false;
+  if (state.value.openConfigurationBar) {
     state.value.openConfigurationBar = false;
   }
-  state.value.backdrop = false;
-});
+}
+
+function toggleConfigurationBar() {
+  state.value.openConfigurationBar = !state.value.openConfigurationBar;
+  if (state.value.openNavigationBar) {
+    state.value.openNavigationBar = false;
+  }
+}
 </script>

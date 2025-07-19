44fb885acf68d24259f26e38142eefdc83eabff7
Coc̀éll <cocell.zen.tepys@gmail.com>
2025-07-19 11:08:52 +0600

    Add whole component wrapper, attribute handling, buit in backdrop, `position` props, and imporved responsiveness

--- packages/core/src/vue/components/Sidebar.vue
+++ packages/core/src/vue/components/Sidebar.vue
@@ -1,29 +1,67 @@
 <template>
-  <div class="sidebar" :class="{ 'sidebar--open': isOpen }" :inert="!isOpen" role="complementary"
-    :aria-expanded="isOpen ? 'true' : 'false'">
-    <div class="content">
-      <slot></slot>
+  <div class="sidebar-wrapper">
+    <div class="backdrop" @click="closeSidebar" :class="{ 'backdrop--show': showBackdrop }"></div>
+    <div class="sidebar" :class="[
+      'sidebar',
+      { 'sidebar--open': isOpen, 'sidebar--left': props.position === 'left', 'sidebar--right': props.position === 'right' },
+      attrs.class
+    ]" v-bind="{ ...attrs, class: undefined }" :inert="!isOpen" :role="'complementary' || attrs.role"
+      :aria-expanded="isOpen ? 'true' : 'false'">
+      <div class="content">
+        <slot></slot>
+      </div>
     </div>
   </div>
 </template>
 
+
 <script setup>
-import { computed } from 'vue'
+defineOptions({ inheritAttrs: false })
+import { computed, defineEmits, useAttrs, ref, onMounted, onUnmounted } from 'vue'
+const attrs = useAttrs();
 
 const props = defineProps({
-  open: Boolean
+  open: {
+    type: Boolean,
+    default: false,
+  },
+  position: {
+    type: String,
+    default: 'left',
+    validator: (value) => ['left', 'right'].includes(value),
+  },
 })
 
-const isOpen = computed(() => props.open)
+const isOpen = computed(() => props.open);
+const screenWidth = ref(window.innerWidth)
+const emit = defineEmits(['open', 'close']);
+const closeSidebar = () => emit('close');
+const openSidebar = () => emit('open');
+const toggleSidebar = () => isOpen.value ? closeSidebar() : openSidebar();
+
+
+const showBackdrop = computed(() => {
+  return screenWidth.value <= 768 && isOpen.value
+})
+
+onMounted(() => window.addEventListener('resize', () => {
+  screenWidth.value = window.innerWidth
+}))
+onUnmounted(() => window.removeEventListener('resize', () => {
+  screenWidth.value = window.innerWidth
+}))
 </script>
 
 <style scoped lang="scss">
+.sidebar-wrapper {
+  height: 100%;
+}
+
 .sidebar {
   width: 0;
   height: 100%;
   overflow: hidden;
   background: var(--surface);
-  border-radius: var(--space-B);
   visibility: hidden;
 
   .content {

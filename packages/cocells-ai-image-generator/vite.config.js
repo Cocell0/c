0c8aa0484e2e7f47652c6c9c37ec913bc92506da
Coc̀éll <cocell.zen.tepys@gmail.com>
2025-07-22 19:27:30 +0600

    Import `perchanceSpecifics` plugin

--- packages/cocells-ai-image-generator/vite.config.js
+++ packages/cocells-ai-image-generator/vite.config.js
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite'
 import UnoCSS from 'unocss/vite'
 import vue from '@vitejs/plugin-vue'
 import { viteSingleFile } from 'vite-plugin-singlefile'
+import { perchanceSpecifics } from 'core'
 
 export default defineConfig({
   plugins: [
     UnoCSS(),
     vue(),
     viteSingleFile(),
+    perchanceSpecifics(),
   ],
 })

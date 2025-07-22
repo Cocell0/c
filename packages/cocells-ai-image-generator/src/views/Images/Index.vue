947c362d7019e2746226956aa985ec4d15af1019
Coc̀éll <cocell.zen.tepys@gmail.com>
2025-07-22 13:31:38 +0600

    Add images view

---
+++ packages/cocells-ai-image-generator/src/views/Images/Index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>View for generated images</p>
+    <a href="#/images/create" class="button">Create New Image</a>
+  </div>
+</template>
+
+<style scoped>
+div {
+  padding: 20px;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>

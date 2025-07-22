d11c428d2e97870f461be43288d8fd0684b956fb
Coc̀éll <cocell.zen.tepys@gmail.com>
2025-07-22 21:45:28 +0600

    Add logic to handle space clicks

--- packages/core/src/vue/components/AnchorLink.vue
+++ packages/core/src/vue/components/AnchorLink.vue
@@ -1,11 +1,11 @@
 <template>
-  <a :href="'/#' + href" :class="{ active: isActive }">
+  <a @keydown="open" ref="anchor" :href="'#' + href" :class="{ active: isActive }">
     <slot />
   </a>
 </template>
 
 <script setup>
-import { computed, toRef } from 'vue'
+import { computed, ref, toRef } from 'vue'
 import { useRoute } from 'vue-router'
 
 const props = defineProps({
@@ -13,10 +13,16 @@ const props = defineProps({
     type: String,
     required: true
   }
-})
+});
 
-const href = toRef(props, 'href')
-const activeRoute = useRoute()
+const anchor = ref(null);
+const href = toRef(props, 'href');
+const activeRoute = useRoute();
 
 const isActive = computed(() => activeRoute.path === href.value)
+
+function open(event) {
+  if (event.key === ' ')
+    anchor.value.click()
+}
 </script>

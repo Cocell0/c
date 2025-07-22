219a64cf5bba650c9b048a3605ee0580876dc4de
Coc̀éll <cocell.zen.tepys@gmail.com>
2025-07-22 19:22:26 +0600

    Create AnchorLink as a simple substitute of router-link for Perchance

---
+++ packages/core/src/vue/components/AnchorLink.vue
@@ -0,0 +1,22 @@
+<template>
+  <a :href="'/#' + href" :class="{ active: isActive }">
+    <slot />
+  </a>
+</template>
+
+<script setup>
+import { computed, toRef } from 'vue'
+import { useRoute } from 'vue-router'
+
+const props = defineProps({
+  href: {
+    type: String,
+    required: true
+  }
+})
+
+const href = toRef(props, 'href')
+const activeRoute = useRoute()
+
+const isActive = computed(() => activeRoute.path === href.value)
+</script>

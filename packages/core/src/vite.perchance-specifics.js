63d1e3b321b8b59eaf5a2b5f4b9b56938825d17f
Coc̀éll <cocell.zen.tepys@gmail.com>
2025-07-22 19:21:45 +0600

    Add Perchance Specifics Vite plugin

---
+++ packages/core/src/vite.perchance-specifics.js
@@ -0,0 +1,25 @@
+let isBuilding = false
+
+/**
+ * Vite plugin
+ * - Injects a script to map implicitly declared Perchance plugins to `window.plugin`
+ * - Cleans URL params in Perchance after initialization
+ *
+ * @returns {import('vite').Plugin}
+ */
+export default function perchanceSpecifics() {
+  return {
+    name: 'perchance-specifics',
+    async transformIndexHtml(html) {
+      const fs = await import('node:fs/promises')
+      const path = await import('node:path')
+      const { fileURLToPath } = await import('url')
+      const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+      const filePath = path.resolve(__dirname, '__mount-plugins.js')
+      const scriptContent = await fs.readFile(filePath, 'utf-8')
+
+      return html.replace(/<\/head>/i, `<script>${scriptContent}</script></head>`)
+    }
+  }
+}

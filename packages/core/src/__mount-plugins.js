08a540344c9a8c0b054e218a291584be0633f3c6
Coc̀éll <cocell.zen.tepys@gmail.com>
2025-07-22 19:26:13 +0600

    Remove plugin scoped script

--- packages/core/src/__mount-plugins.js
+++
@@ -1,8 +0,0 @@
-/**
-  - Adds all plugins to the global `window.plugin` object
-  - This is used to ensure that plugins are available globally in the browser environment
-*/
-window.plugins = {}
-$plugins.$allKeys.forEach(key => {
-  window.plugin[key] = $plugin[key]
-})

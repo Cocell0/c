3a21a5406c9f4f097a25e17edab23b7cbef16852
Coc̀éll <cocell.zen.tepys@gmail.com>
2025-07-22 21:36:13 +0600

    Add `appName`

--- packages/core/src/vite.perchance-specifics.js
+++ packages/core/src/vite.perchance-specifics.js
@@ -1,5 +1,3 @@
-let isBuilding = false
-
 /**
  * Vite plugin
  * - Injects a script to map implicitly declared Perchance plugins to `window.plugin`
@@ -7,19 +5,20 @@ let isBuilding = false
  *
  * @returns {import('vite').Plugin}
  */
-export default function perchanceSpecifics() {
+export default function perchanceSpecifics(name) {
   return {
     name: 'perchance-specifics',
     async transformIndexHtml(html) {
-      const fs = await import('node:fs/promises')
-      const path = await import('node:path')
-      const { fileURLToPath } = await import('url')
-      const __dirname = path.dirname(fileURLToPath(import.meta.url))
+      const fs = await import('node:fs/promises');
+      const path = await import('node:path');
+      const { fileURLToPath } = await import('url');
+      const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-      const filePath = path.resolve(__dirname, 'perchance-specifics.js')
-      const scriptContent = await fs.readFile(filePath, 'utf-8')
+      const filePath = path.resolve(__dirname, 'perchance-specifics.js');
+      const scriptContent = await fs.readFile(filePath, 'utf-8');
+      const updatedScriptContent = scriptContent.replace('const appName = null', `const appName = '${name}'`);
 
-      return html.replace(/<\/head>/i, `<script>${scriptContent}</script></head>`)
+      return html.replace(/<\/head>/i, `<script>${updatedScriptContent}</script></head>`)
     }
   }
 }

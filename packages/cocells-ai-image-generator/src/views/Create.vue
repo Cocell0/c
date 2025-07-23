a95a31eb3205e58f2dfca2de41e4d4af2be25fda
Coc̀éll <cocell.zen.tepys@gmail.com>
2025-07-23 19:41:30 +0600

    Remove redundant functions, logic, and import `useUiStore` to improve maintainability

--- packages/cocells-ai-image-generator/src/views/Create.vue
+++ packages/cocells-ai-image-generator/src/views/Create.vue
@@ -2,15 +2,14 @@
   <div class="drawer" role="toolbar">
     <DarkMode />
     <button @click="toggleConfigurationBar" class="button--icon"
-      :aria-label="state.openConfigurationBar ? 'Close configuration' : 'Open configuration'">
+      :aria-label="uiStore.isConfigurationBarOpen ? 'Close configuration' : 'Open configuration'">
       <span
-        :class="{ 'i-material-symbols-close-rounded': state.openConfigurationBar, 'i-material-symbols-settings-rounded': !state.openConfigurationBar }"></span>
+        :class="{ 'i-material-symbols-close-rounded': uiStore.isConfigurationBarOpen, 'i-material-symbols-settings-rounded': !uiStore.isConfigurationBarOpen }"></span>
     </button>
   </div>
   <MainContent @toggle-configuration-bar="toggleConfigurationBar" />
   <div class="panel">
-    <Sidebar ref="configurationSidebar" @close="toggleConfigurationBar" :open="state.openConfigurationBar"
-      class="configuration-bar relative" position="right">
+    <Sidebar @close="uiStore.isConfigurationBarOpen = false" :open="uiStore.isConfigurationBarOpen" class="configuration-bar relative" position="right">
       <h3 class="my-2.5rem text-center">Configuration</h3>
       <AnchorLink href="/images" class="button">Images</AnchorLink>
     </Sidebar>
@@ -18,37 +17,16 @@
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue';
 import DarkMode from 'core/src/vue/components/DarkMode.vue';
 import AnchorLink from 'core/src/vue/components/AnchorLink.vue';
 import Sidebar from 'core/src/vue/components/Sidebar.vue';
 import MainContent from '@/components/MainContent.vue';
-import { ref, onMounted, computed } from 'vue';
+import { useUiStore } from '@/store/useUiStore';
 
-const state = ref({
-  openNavigationBar: false,
-  openConfigurationBar: false,
-  screenWidth: ref(window.innerWidth),
-});
-
-onMounted(() => window.addEventListener('resize', () => {
-  state.value.screenWidth = window.innerWidth
-}))
-
-const hideMenuButton = computed(() =>
-  state.value.screenWidth <= 768 && state.value.openConfigurationBar
-)
-
-function toggleNavigationBar() {
-  state.value.openNavigationBar = !state.value.openNavigationBar;
-  if (state.value.openConfigurationBar) {
-    state.value.openConfigurationBar = false;
-  }
-}
+const uiStore = useUiStore();
 
 function toggleConfigurationBar() {
-  state.value.openConfigurationBar = !state.value.openConfigurationBar;
-  if (state.value.openNavigationBar) {
-    state.value.openNavigationBar = false;
-  }
+  uiStore.isConfigurationBarOpen = !uiStore.isConfigurationBarOpen;
 }
 </script>

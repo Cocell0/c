cf8c82b98635d520594fb4b14b31374f53284b19
Coc̀éll <cocell.zen.tepys@gmail.com>
2025-07-24 02:27:28 +0600

    Modularized code to improve future maintainability

--- packages/cocells-ai-image-generator/src/views/Create.vue
+++ packages/cocells-ai-image-generator/src/views/Create.vue
@@ -1,23 +1,27 @@
 <template>
-  <div class="drawer" role="toolbar">
-    <DarkMode />
-    <button @click="toggleConfigurationBar" class="button--icon"
-      :aria-label="uiStore.isConfigurationBarOpen ? 'Close configuration' : 'Open configuration'">
-      <span
-        :class="{ 'i-material-symbols-close-rounded': uiStore.isConfigurationBarOpen, 'i-material-symbols-settings-rounded': !uiStore.isConfigurationBarOpen }"></span>
-    </button>
-  </div>
-  <MainContent @toggle-configuration-bar="toggleConfigurationBar" />
-  <div class="panel">
-    <Sidebar @close="uiStore.isConfigurationBarOpen = false" :open="uiStore.isConfigurationBarOpen"
-      class="configuration-bar relative" position="right">
-      <h3 class="my-2.5rem text-center">Configuration</h3>
-      <AnchorLink href="/images" class="button">Images</AnchorLink>
-      <label for="size">Size:</label>
-      <select name="size" id="size">
-        <option v-for="size in configurationStore.size" :key="size.name" :value="size.name">{{ size.name }}</option>
-      </select>
-    </Sidebar>
+  <div class="router-wrapper">
+    <div role="toolbar">
+      <DarkMode />
+      <button @click="toggleConfigurationBar" class="button--icon"
+        :aria-label="uiStore.isConfigurationBarOpen ? 'Close configuration' : 'Open configuration'">
+        <span
+          :class="{ 'i-material-symbols-close-rounded': uiStore.isConfigurationBarOpen, 'i-material-symbols-settings-rounded': !uiStore.isConfigurationBarOpen }"></span>
+      </button>
+    </div>
+    <div class="app-content">
+      <MainContent />
+      <div class="panel">
+        <Sidebar @close="uiStore.isConfigurationBarOpen = false" :open="uiStore.isConfigurationBarOpen"
+          class="configuration-bar relative" position="right">
+          <h3 class="my-2.5rem text-center">Configuration</h3>
+          <AnchorLink href="/images" class="button">Images</AnchorLink>
+          <label for="size">Size:</label>
+          <select name="size" id="size">
+            <option v-for="size in configurationStore.size" :key="size.name" :value="size.name">{{ size.name }}</option>
+          </select>
+        </Sidebar>
+      </div>
+    </div>
   </div>
 </template>
 

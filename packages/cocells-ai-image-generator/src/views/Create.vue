824f3614af1873636a0794a9df8ec6da4c2cec99
Coc̀éll <cocell.zen.tepys@gmail.com>
2025-07-23 21:52:17 +0600

    Import configuration store

--- packages/cocells-ai-image-generator/src/views/Create.vue
+++ packages/cocells-ai-image-generator/src/views/Create.vue
@@ -9,22 +9,28 @@
   </div>
   <MainContent @toggle-configuration-bar="toggleConfigurationBar" />
   <div class="panel">
-    <Sidebar @close="uiStore.isConfigurationBarOpen = false" :open="uiStore.isConfigurationBarOpen" class="configuration-bar relative" position="right">
+    <Sidebar @close="uiStore.isConfigurationBarOpen = false" :open="uiStore.isConfigurationBarOpen"
+      class="configuration-bar relative" position="right">
       <h3 class="my-2.5rem text-center">Configuration</h3>
       <AnchorLink href="/images" class="button">Images</AnchorLink>
+      <label for="size">Size:</label>
+      <select name="size" id="size">
+        <option v-for="size in configurationStore.size" :key="size.name" :value="size.name">{{ size.name }}</option>
+      </select>
     </Sidebar>
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
 import DarkMode from 'core/src/vue/components/DarkMode.vue';
 import AnchorLink from 'core/src/vue/components/AnchorLink.vue';
 import Sidebar from 'core/src/vue/components/Sidebar.vue';
 import MainContent from '@/components/MainContent.vue';
 import { useUiStore } from '@/store/useUiStore';
+import { useConfigurationStore } from '@/store/useConfigurationStore';
 
 const uiStore = useUiStore();
+const configurationStore = useConfigurationStore();
 
 function toggleConfigurationBar() {
   uiStore.isConfigurationBarOpen = !uiStore.isConfigurationBarOpen;

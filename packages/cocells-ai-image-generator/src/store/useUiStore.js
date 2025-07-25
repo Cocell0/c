024efcc22309e74722586f9b8a92a418e026db79
Coc̀éll <cocell.zen.tepys@gmail.com>
2025-07-25 21:36:53 +0600

    Change property to reflect design change

--- packages/cocells-ai-image-generator/src/store/useUiStore.js
+++ packages/cocells-ai-image-generator/src/store/useUiStore.js
@@ -2,6 +2,6 @@ import { defineStore } from "pinia";
 
 export const useUiStore = defineStore("ui", {
   state: () => ({
-    isConfigurationBarOpen: false,
+    isConfigurationOpen: false,
   }),
 });

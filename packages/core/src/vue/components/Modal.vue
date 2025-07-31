79d8710650cde7abbb76814978d710c1f4915459
Coc̀éll <cocell.zen.tepys@gmail.com>
2025-07-31 16:56:48 +0600

    Adopt C̀-style

--- packages/core/src/vue/components/Modal.vue
+++ packages/core/src/vue/components/Modal.vue
@@ -106,7 +106,7 @@ onMounted(() => {
   }
 
   >*:not(:last-child) {
-    border-bottom: 1px solid var(--divider);
+    border-bottom: 1px solid var(--color-border-divider-opaque);
   }
 
   .head {

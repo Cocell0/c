6b119c28c6fbde1aaafe52ce513e17dbb3559048
Coc̀éll <cocell.zen.tepys@gmail.com>
2025-11-15 22:05:54 +0600

    Add main.js file to initialize Vue application with styles and components

---
+++ packages/custom-chat/src/main.js
@@ -0,0 +1,10 @@
+import 'core/src/font.css'
+import 'c-style'
+import './assets/main.scss'
+import 'core/src/paramount.css'
+
+import { createApp } from 'vue';
+import App from './App.vue';
+
+const app = createApp(App);
+app.mount('#app')

74384758c439c58ea2142aff78b18258f92f23ac
Coc̀éll <cocell.zen.tepys@gmail.com>
2025-11-16 21:54:43 +0600

    Enhance main.js to integrate Vue Router and Pinia for state management

--- packages/custom-chat/src/main.js
+++ packages/custom-chat/src/main.js
@@ -3,8 +3,12 @@ import 'c-style'
 import './assets/main.scss'
 import 'core/src/paramount.css'
 
-import { createApp } from 'vue';
-import App from './App.vue';
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+import router from './router'
 
 const app = createApp(App);
+app.use(router)
+app.use(createPinia())
 app.mount('#app')

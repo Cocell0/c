493047efb17554187141e78d84a48462bb6e0f94
Coc̀éll <cocell.zen.tepys@gmail.com>
2025-11-17 13:32:41 +0600

    Add Home view template to custom-chat package

---
+++ packages/custom-chat/src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <h2>Home</h2>
+  <p>
+    Go to <AnchorLink href="/c">Chat</AnchorLink>
+    <br>
+    Or go <AnchorLink href="/">Home</AnchorLink>
+  </p>
+</template>
+
+<script setup>
+import AnchorLink from 'core/src/vue/components/AnchorLink.vue'
+</script>

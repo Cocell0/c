86aa1cd7c999ee952b272ccf9e1f28f98963fdc2
Coc̀éll <cocell.zen.tepys@gmail.com>
2025-11-18 19:05:23 +0600

    Refactor Chat component to enhance layout and state management

--- packages/custom-chat/src/views/Chat.vue
+++ packages/custom-chat/src/views/Chat.vue
@@ -1,10 +1,32 @@
 <template>
-  Chat {{ id }}
+  <main>
+    <ul class="chat-tabs">
+      <li>
+        <AnchorLink href="/c" class="button">All chats</AnchorLink>
+      </li>
+      <li v-for="chat in chats" :key="chat.id">
+        <AnchorLink :href="`/c/${chat.id}`" class="button">{{ chat.name }}</AnchorLink>
+      </li>
+    </ul>
+    <div v-if="id">
+      <h2>{{ chat.name }}</h2>
+      <!-- Chat content would go here -->
+    </div>
+  </main>
 </template>
 
 <script setup>
+import { computed } from 'vue';
 import { useRoute } from 'vue-router';
+import { useChatsStore } from '../store/useChatsStore';
+import AnchorLink from 'core/src/vue/components/AnchorLink.vue';
 
 const route = useRoute();
-const id = route.params.id;
+const id = computed(() => route.params.id);
+
+const chatsStore = useChatsStore();
+console.log(chatsStore.globalChats);
+
+const chats = computed(() => [...chatsStore.globalChats, ...chatsStore.savedChats])
+const chat = computed(() => chats.value.find(chat => chat.id === id.value))
 </script>

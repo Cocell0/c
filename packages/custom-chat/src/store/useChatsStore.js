0935e1559a6d9d90c49e0b69ca0f94b3c54a78da
Coc̀éll <cocell.zen.tepys@gmail.com>
2025-11-18 19:05:12 +0600

    Add useChatsStore for managing chat state

---
+++ packages/custom-chat/src/store/useChatsStore.js
@@ -0,0 +1,21 @@
+import { defineStore } from "pinia";
+
+export const useChatsStore = defineStore("chats", {
+  state: () => ({
+    globalChats: [
+      {
+        name: "General",
+        id: "general",
+      },
+      {
+        name: "Living Room",
+        id: "living-room",
+      },
+      {
+        name: "Garden",
+        id: "garden",
+      },
+    ],
+    savedChats: [],
+  }),
+});

2061aded513f35ea7dcac369b91aa696e3935fea
Coc̀éll <cocell.zen.tepys@gmail.com>
2025-11-20 12:42:47 +0600

    Add ChatInterface.vue for Perchance Comment's plugin

---
+++ packages/custom-chat/src/components/ChatInterface.vue
@@ -0,0 +1,13 @@
+<template>
+  {{ config }}
+</template>
+<script setup>
+import { defineProps } from 'vue';
+
+defineProps({
+  config: {
+    type: Object,
+    required: true
+  },
+});
+</script>

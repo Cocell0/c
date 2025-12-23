f75a81fd307934510e8c66d3c6f1366de26cce58
Coc̀éll <cocell.zen.tepys@gmail.com>
2025-12-23 16:07:39 +0600

    Refactor "add" button into separate component

---
+++ packages/custom-chat/src/components/Add.vue
@@ -0,0 +1,10 @@
+<template>
+  <button class="button--icon">
+    <span
+      class="i-material-symbols:add-2-rounded"
+      aria-hidden="true"
+      translate="no"
+      inert
+    ></span>
+  </button>
+</template>

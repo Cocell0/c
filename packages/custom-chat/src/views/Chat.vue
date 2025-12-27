73e5a45291ab8a9a4c8d2cc7dd8942f2faced4f2
Coc̀éll <cocell.zen.tepys@gmail.com>
2025-12-27 19:46:00 +0600

    Disable KeepAlive router-view experiment
    - Comment out KeepAlive router-view block
    - Restore plain router-view rendering

--- packages/custom-chat/src/views/Chat.vue
+++ packages/custom-chat/src/views/Chat.vue
@@ -67,6 +67,11 @@
       </div>
     </section>
 
+    <!-- <router-view v-slot="{ Component }">
+      <keep-alive>
+        <component :is="Component" />
+      </keep-alive>
+    </router-view> -->
     <router-view />
   </main>
 </template>

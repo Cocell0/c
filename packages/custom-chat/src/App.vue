70e376c7a52336dc61ba52bb3228a3dc55fd6904
Coc̀éll <cocell.zen.tepys@gmail.com>
2026-01-11 14:44:52 +0600

    Add hidden ThemeSwitcher to sync theme across routes

--- packages/custom-chat/src/App.vue
+++ packages/custom-chat/src/App.vue
@@ -1,3 +1,8 @@
 <template>
   <router-view />
+  <!-- Defining ThemeSwitcher here to update the theme when the user switches themes -->
+  <ThemeSwitcher style="position: fixed; visibility: hidden" inert />
 </template>
+<script setup>
+import ThemeSwitcher from "core/src/vue/components/ThemeSwitcher.vue";
+</script>

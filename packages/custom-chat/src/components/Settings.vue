36a0104a2604fde1cb6bbe88e45eb6f43a5d1ce2
Coc̀éll <cocell.zen.tepys@gmail.com>
2026-01-14 20:32:56 +0600

    Create Settings component with modal
    - Added button to open settings modal
    - Modal shows placeholder content

---
+++ packages/custom-chat/src/components/Settings.vue
@@ -0,0 +1,24 @@
+<template>
+  <button
+    class="button--icon"
+    @click="isOpen = true"
+    aria-label="Open settings"
+  >
+    <span class="i-material-symbols:settings-rounded"></span>
+  </button>
+  <Modal v-model:open="isOpen" title="Settings">
+    <p>Settings contents</p>
+  </Modal>
+</template>
+<script setup>
+import { ref } from "vue";
+import Modal from "core/src/vue/components/Modal.vue";
+
+const isOpen = ref(false);
+</script>
+<style scoped lang="scss">
+.modal {
+  max-width: clamp(300px, 90%, 68dvw);
+  max-height: clamp(200px, 80%, 92dvh);
+}
+</style>

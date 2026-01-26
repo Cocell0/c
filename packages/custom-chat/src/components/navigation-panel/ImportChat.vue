a56d982f4e8d12ebe5834a7bd89ae595c83afd4f
Coc̀éll <cocell.zen.tepys@gmail.com>
2026-01-26 21:22:50 +0600

    Add accessibility attributes to download icon
    - Set `aria-hidden` and `inert` on the icon inside Import chat button

--- packages/custom-chat/src/components/navigation-panel/ImportChat.vue
+++ packages/custom-chat/src/components/navigation-panel/ImportChat.vue
@@ -1,5 +1,5 @@
 <template>
   <button class="button--icon" aria-label="Import chat" title="Import chat">
-    <span class="i-material-symbols:download-rounded"></span>
+    <span class="i-material-symbols:download-rounded" aria-hidden inert></span>
   </button>
 </template>

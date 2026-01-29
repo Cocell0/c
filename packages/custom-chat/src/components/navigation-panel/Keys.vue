542a220c1fef803eb47a27556e5f93cc65b0a31f
Coc̀éll <cocell.zen.tepys@gmail.com>
2026-01-29 21:50:09 +0600

    Add Keys.vue

---
+++ packages/custom-chat/src/components/navigation-panel/Keys.vue
@@ -0,0 +1,69 @@
+<template>
+  <button
+    class="button--icon"
+    @click="isOpen = true"
+    aria-label="Open keys"
+    title="Open keys"
+  >
+    <span class="i-material-symbols:key-rounded"></span>
+  </button>
+  <Modal v-model:open="isOpen" title="Keys">
+    <div>
+      <label for="public-key-field">Public key:</label>
+      <input
+        id="public-key-field"
+        v-model="publicKey"
+        placeholder="Public Key"
+        readonly
+      />
+    </div>
+    <div>
+      <label for="private-key-field">Private key:</label>
+      <input
+        id="private-key-field"
+        v-model="privateKey"
+        placeholder="Private Key"
+        readonly
+      />
+    </div>
+  </Modal>
+</template>
+<script setup>
+import { ref } from "vue";
+import Modal from "core/src/vue/components/Modal.vue";
+import { useSecretsStore } from "@/stores/useSecretsStore";
+
+const secretsStore = useSecretsStore();
+
+const { publicKey, privateKey } = secretsStore;
+const isOpen = ref(false);
+</script>
+<style scoped lang="scss">
+.modal {
+  .body {
+    display: flex;
+    flex-direction: column;
+    gap: var(--spacing--B);
+
+    label {
+      font-weight: bold;
+      display: block;
+    }
+
+    input {
+      width: 100%;
+      font-family: var(--font-mono);
+      margin-bottom: var(--spacing--C);
+      -webkit-text-security: disc;
+      &:focus {
+        -webkit-text-security: unset;
+      }
+      @media (hover: hover) {
+        &:hover {
+          -webkit-text-security: unset;
+        }
+      }
+    }
+  }
+}
+</style>

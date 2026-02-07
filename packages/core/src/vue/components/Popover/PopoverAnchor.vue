ac11f6b8ca1e5797acad1023091141f3017cca1b
Coc̀éll <cocell.zen.tepys@gmail.com>
2026-02-07 17:55:34 +0600

    Add PopoverAnchor component to provide anchor element
    - Uses `inject` to access Popover context
    - Assigns local `ref` to Popover's `anchorElement`
    - Inline-block styling for proper positioning

---
+++ packages/core/src/vue/components/Popover/PopoverAnchor.vue
@@ -0,0 +1,22 @@
+<template>
+  <span ref="anchorElement" class="popover__anchor">
+    <slot />
+  </span>
+</template>
+
+<script setup>
+import { ref, inject, onMounted } from "vue";
+
+const anchorElement = ref(null);
+const popover = inject("popover");
+
+onMounted(() => {
+  popover.anchorElement.value = anchorElement.value;
+});
+</script>
+
+<style lang="scss" scoped>
+.popover__anchor {
+  display: inline-block;
+}
+</style>
